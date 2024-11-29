d10ba3d3f63bc178e8cb67004fe69de6a92bf048
saadelkharraz <saad.elkharraz@etu.uae.ac.ma>
2024-11-29 18:32:37 +0100

    server side configuration + get routes
    ...

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'Frontend')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Frontend', 'LandingPage.html')); 
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Frontend', 'LoginRegister.html')); 
+});
+app.get('/users', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Frontend', 'Users.html')); 
+});
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Frontend', 'Admin.html')); 
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
