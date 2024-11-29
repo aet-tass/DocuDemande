b3ea637f0d1999dffa745a5dff79fe391308f99f
saadelkharraz <saad.elkharraz@etu.uae.ac.ma>
2024-11-29 19:11:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ const port = 3000;
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'Frontend')));
 
+// Créer une connexion à la base de données
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',     
+    password: '',     
+    database: 'docu_demande'  
+  });
+// Connecter à MySQL
+db.connect(err => {
+    if (err) {
+      console.error('Erreur de connexion :', err);
+      return;
+    }
+    console.log('Connecté à la base de données MySQL');
+  });
+
 //Get requests :
 
 app.get('/', (req, res) => {
