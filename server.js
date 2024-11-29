d86ab22f71448bd789957552ca7d036eeda619dd
saadelkharraz <saad.elkharraz@etu.uae.ac.ma>
2024-11-29 21:07:22 +0100

    ..

--- server.js
+++ server.js
@@ -2,23 +2,57 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, 'Frontend')));
 
-// Créer une connexion à la base de données
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',     
-    password: '',     
-    database: 'docu_demande'  
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/docudemande')
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
   });
-// Connecter à MySQL
-db.connect(err => {
-    if (err) {
-      console.error('Erreur de connexion :', err);
-      return;
-    }
-    console.log('Connecté à la base de données MySQL');
-  });
+
+//user
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  cin: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+}, { timestamps: true });
+const User = mongoose.model('User', userSchema);
+
 
 //Get requests :
 
@@ -34,6 +68,50 @@ app.get('/users', (req, res) => {
 app.get('/admin', (req, res) => {
     res.sendFile(path.join(__dirname, 'Frontend', 'Admin.html')); 
 });
+
+//Post
+// Route to handle registration
+app.post('/register', async (req, res) => {
+  console.log(req.body);
+  const { firstName, lastName, email, address, phone, cin, password, confirmPassword } = req.body;
+
+  // Check if passwords match
+  if (password !== confirmPassword) {
+    return res.status(400).send("Passwords don't match");
+  }
+
+  try {
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).send("User already exists");
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create new user
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      address,
+      phone,
+      cin,
+      password: hashedPassword
+    });
+
+    // Save user to the database
+    await newUser.save();
+
+    // Send success response
+    res.status(201).send('User registered successfully!');
+  } catch (error) {
+    console.error("Registration error:", error);
+    res.status(500).send('Error during registration');
+  }
+});
+
 
 
 app.listen(port, () => {
